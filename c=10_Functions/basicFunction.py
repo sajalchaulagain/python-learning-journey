c56bb3601811b49bbdf8ec6d3c75d3264b44ec4c
Sajal Chaulagain <chaulagainsajal94@gmail.com>
2026-06-15 15:44:42 +0545

    python-learnign_started with function

---
+++ c=10_Functions/basicFunction.py
@@ -0,0 +1,9 @@
+def greet ():
+    print("Hello world")
+
+    # calling function
+    greet()
+
+
+# Real life example: we created a machine called greet. 
+# whenever we press the button (greet()), it says Hello.
---
+++ c=10_Functions/funtionWithParameters.py
@@ -0,0 +1,1 @@
+# parameters are inputs.
